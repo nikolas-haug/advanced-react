97aae4139babcaff7454c643dcc22859cba06e73
Nikolas Haug <nikohaug21@gmail.com>
2020-11-16 16:21:08 -0600

    initial commit

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random text');
+
+  const handleClick = () => {
+    if(text === 'random text') {
+      setText('hello world');
+    } else {
+      setText('random text');
+    }
+  }
+  return (
+      <React.Fragment>
+        <h1>{text}</h1>
+        <button className="btn" onClick={handleClick}>change title</button>
+      </React.Fragment>
+    )
 };
 
 export default UseStateBasics;
